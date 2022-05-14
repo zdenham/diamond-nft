5b99d1299e540bc3f2a0cbb4797b486249f294e1
zdenham <zdenham1@gmail.com>
2022-05-13 17:47:04 -0700

    move repo

--- contracts/facets/DiamondClone/BaseDiamondCloneFacet.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {DiamondCloneCutFacet} from "./DiamondCloneCutFacet.sol";
-import {DiamondCloneLoupeFacet} from "./DiamondCloneLoupeFacet.sol";
-
-contract BaseDiamondCloneFacet is DiamondCloneCutFacet, DiamondCloneLoupeFacet {}
